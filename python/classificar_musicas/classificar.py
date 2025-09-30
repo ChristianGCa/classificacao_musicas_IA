e6d58f6845b764ed037cc7af77e4135da4bd69a9
ChristianGCa <candelonichristian@gmail.com>
2025-09-29 22:37:38 -0300

    Adicionando a funcionalidade de processar as músicas aos poucos, salvando em outro csv as músicas já processadas

--- python/classificar_musicas/classificar.py
+++ python/classificar_musicas/classificar.py
@@ -1,5 +1,10 @@
 import ollama
 import pandas as pd
+import os
+
+dataset_csv = "/home/christian/Downloads/spotify_millsongdata_teste_30.csv"
+classified_csv = "/home/christian/Documentos/spotify_millsongdata_CLASSIFICADO.csv"
+
 
 def classificar_letra(letra: str) -> str:
     prompt = f"""
@@ -12,18 +17,35 @@ Música:
     response = ollama.generate(model="mistral:latest", prompt=prompt)
     return response["response"].strip().lower()
 
-print("Tentando ler")
 
-df = pd.read_csv("/home/christian/Downloads/spotify_millsongdata_teste_30.csv")
+if os.path.exists(classified_csv):
+    print("Lendo arquivo de saída existente")
+    classified_songs_df = pd.read_csv(classified_csv)
+    processed_lines = len(classified_songs_df)
+    print(f"Músicas processadas no arquivo: {processed_lines}")
+else:
+    print("Criando novo arquivo de saída")
+    classified_songs_df = pd.DataFrame(columns=["artist", "song", "text", "feeling"])
+    classified_songs_df.to_csv(classified_csv, index=False)
+    processed_lines = 0
 
-print("leu")
+print("Lendo dataset")
+df = pd.read_csv(dataset_csv)
 
-resultados = []
-for i, letra in enumerate(df["text"], start=1):
-    sentimento = classificar_letra(letra)
-    resultados.append(sentimento)
-    print(f"{i}/{len(df)}", " | ", df["song"].iloc[i-1], " - ", sentimento)
+results = []
+for i in range (processed_lines, len(df)):
+    text = df["text"].iloc[i]
+    feeling = classificar_letra(text)
+    results.append(feeling)
+    print(f"{i}/{len(df)}", " | ", df["song"].iloc[i-1], " - ", feeling)
 
-df["sentimento"] = resultados
+    new_line = {
+        "artist": df["artist"].iloc[i],
+        "song": df["song"].iloc[i],
+        "text": text,
+        "feeling": feeling
+    }
 
-print(df[["artist", "song", "sentimento"]])
+    pd.DataFrame([new_line]).to_csv(classified_csv, mode="a", header=False, index=False)
+
+print(df[["artist", "song", "feeling"]])
