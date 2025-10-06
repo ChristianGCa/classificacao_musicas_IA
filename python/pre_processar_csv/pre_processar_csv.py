431a3b5e092f0d8e5b99d700f9490fb20f64a9d5
ChristianGCa <candelonichristian@gmail.com>
2025-10-06 19:44:54 -0300

    melhorando a lógica de limpeza de dados

--- python/pre_processar_csv/pre_processar_csv.py
+++ python/pre_processar_csv/pre_processar_csv.py
@@ -1,14 +1,21 @@
 # Programa para remover as quebras de linha
 
 import pandas as pd
+import re
 
 arquivo_de_entrada = '/home/christian/Downloads/spotify_millsongdata.csv'
 arquivo_de_saida = '/home/christian/Documentos/CSVs_processados/spotify_millsongdata_SEM_QUEBRAS_LINHA.csv'
+
+def clean_text(text):
+    text = re.sub(r'[-.,!?;:“”‘’]', ' ', text) # Substitui pontuação por espaço
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
 
 try:
     df = pd.read_csv(arquivo_de_entrada)
     if 'text' in df.columns:
         df['text'] = df['text'].str.replace('\n', ' ', regex=False)
+        df['text'] = df['text'].apply(clean_text)
         df.to_csv(arquivo_de_saida, index=False)
 
         print("CSV processado e salvo")
